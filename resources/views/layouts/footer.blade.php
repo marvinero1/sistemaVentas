d526149ea0a202034d228356d1e6866576390902
marvinero1 <marvinero@gmail.com>
2020-12-09 14:23:13 -0400

    fisrt commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2021 <a href="https://fullpro.es" target="_blanck">Full Pro Technology</a>.</strong>
+    Todos los derechos reservados.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 2.0.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
